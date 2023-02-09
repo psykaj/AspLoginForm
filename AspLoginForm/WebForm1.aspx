27b64a06e92a621651e79ae8a1bafecf8d27a8d3
Pankaj Yadav <pankajdark6@gmail.com>
2023-02-09 19:22:55 +0530

    loginForm form using asp.net

---
+++ AspLoginForm/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="AspLoginForm.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login page using asp.net</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            UserName :
+            <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Password :
+            <asp:TextBox ID="txtPassword" TextMode="Password" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="lblMessage" runat="server" Text="Label" Visible="False"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ AspLoginForm/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AspLoginForm
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source = DESKTOP-P8FARIT\SQLEXPRESS; Initial Catalog = loginUserAccount; Integrated Security = True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Reference Video = https://youtu.be/AvrQS6df7L8 for login form
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+            SqlDataAdapter sqlda = new SqlDataAdapter("select * from userAccountTable where UserName = ' "+ txtUserName.Text +" ' and Password = ' "+ txtPassword.Text + " ' " , sqlCon);
+            //from here we put validations
+            DataTable dt = new DataTable();
+            sqlda.Fill(dt);
+
+            if(dt.Rows.Count > 0)
+            {
+                lblMessage.Visible = true;
+                lblMessage.Text = "You're Successfully login to the system!";
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblMessage.Visible = true;
+                lblMessage.Text = "Invalid UserName or Password!";
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
